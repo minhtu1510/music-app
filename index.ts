c001b57164f9d7e88c43b6acc760c56d87fb2735
minhtu1510 <thaithiminhtu.338@gmail.com>
2024-11-18 15:22:07 +0700

    Cài đặt pug

--- index.ts
+++ index.ts
@@ -3,8 +3,15 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number = 3000;
 
+
+// serve static files from the `public` folder
+app.set("views",`${__dirname}/views`)
+// add view template engine
+app.set('view engine', 'pug')
+
+
 app.get("/topics", (req: Request, res: Response) => {
-  res.send("Chủ đề bài hát");
+  res.render("client/pages/topics/index");
 });
 
 app.listen(port, () => {
