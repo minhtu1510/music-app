b4571792e07aed93b6681d96703be01ec995a4db
minhtu1510 <thaithiminhtu.338@gmail.com>
2024-11-20 00:18:39 +0700

    Cấu trúc lại file views

--- controllers/topic.controller.ts
+++ controllers/topic.controller.ts
@@ -6,5 +6,7 @@ export const index = async (req: Request, res: Response) => {
   });
   console.log(topics);
 
-  res.render("client/pages/topics/index");
+  res.render("client/pages/topics/index", {
+    pageTitle: "Chủ đề bài hát",
+  });
 };
