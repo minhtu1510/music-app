09df57fa86423d19fa33b7e6c2cbc7bae8b5c4e1
minhtu1510 <thaithiminhtu.338@gmail.com>
2024-12-06 11:57:18 +0700

    Trang quản lý chủ đề

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,7 +1,9 @@
 import { Express } from "express";
 import { dashboardRoute } from "./dashboard.route";
+import { topicRoute } from "./topic.route";
 import { systemConfig } from "../../config/system";
 export const routesAdmin = (app: Express) => {
   const path = systemConfig.prefixAdmin;
   app.use(`/${path}/dashboard`, dashboardRoute);
+  app.use(`/${path}/topics`, topicRoute);
 };
---
+++ routes/admin/topic.route.ts
@@ -0,0 +1,5 @@
+import express from "express";
+const router = express.Router();
+import * as controller from "../../controllers/admin/topic.controller";
+router.get("/", controller.index);
+export const topicRoute = router;
