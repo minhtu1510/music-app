0ec67f1c0f4a92a60d568769c4c4df5552f6e51f
unknown <thaolinh252512@gmail.com>
2024-12-31 22:22:09 +0700

    playlist và hoàn thiện thêm giao diện

--- controllers/playlist.controller.ts
+++ controllers/playlist.controller.ts
@@ -1,14 +1,67 @@
 import { Request, Response } from "express";
-import { Song } from "../models/song.model";
-import { Topic } from "../models/topic.model";
 import { Singer } from "../models/singer.model";
-import { FavoriteSong } from "../models/favorite-song.model";
-import unidecode from "unidecode";
-import { title } from "process";
-import moment from "moment";
-
+import { Playlist } from "../models/playlist.model";
+import { User } from "../models/user.model";
 export const index = async (req: Request, res: Response) => {
+    const userId = res.locals.users.id
+    const playlists = await Playlist.find(
+        {
+            userId: userId
+        }
+    )
+    const user = await User.findOne({
+        _id: userId
+    })
     res.render("client/pages/playlist/index", {
-        pageTitle: "Bài hát yêu thích"
+        pageTitle: "Bài hát yêu thích",
+        playlists: playlists,
+        userName: user.fullName
     });
+};
+
+export const createPlaylist = async (req: Request, res: Response) => {
+    const newTitlePlaylist = req.body.namePlaylist;
+    console.log(newTitlePlaylist);
+    const userId = res.locals.users.id
+    const newPlaylist = new Playlist({
+        title: newTitlePlaylist,
+        userId: userId
+    });
+    await newPlaylist.save();
+    req.flash("success", "Tạo playlist thành công!");
+    res.redirect("/");
+};
+
+export const addSongPlaylist = async (req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
+
+    }
+    const songId = req.params.songId;
+    const playlistId = req.body.id;
+    console.log(playlistId)
+    const exsitSong = await Playlist.findOne({ _id: playlistId, songId: songId })
+    if (!exsitSong) {
+        await Playlist.updateOne({
+            _id: playlistId
+        },
+            {
+                $push: {
+                    songId: songId
+                }
+            }
+        )
+        res.json({
+            code: "success",
+            message: "Thêm vào playlist thành công",
+        });
+    }
+    else {
+        res.json({
+            code: "error",
+
+        });
+    }
+
 };
