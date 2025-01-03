9c2e6a8c9427bf062b82cea2d5ae2888d0141edf
unknown <thaolinh252512@gmail.com>
2025-01-03 10:03:22 +0700

    chỉnh sửa search - login

--- routes/client/playlist.route.ts
+++ routes/client/playlist.route.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 import * as controller from "../../controllers/playlist.controller";
 
 router.get("/", controller.index);
+router.get("/detail", controller.detail);
 router.post("/", controller.createPlaylist);
 router.patch("/addSong/:songId", controller.addSongPlaylist);
 export const playlistRoute = router;
